56f2dbff96f74d18273afe846b3cfb0139ba5fd4
brushurteeth 8 <adem.hameed.123@gmail.com>
2024-02-12 00:20:59 +0100

    all the task

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var lastName = "Johnson"; //string
 
 var isGreaterThan10 = length > 10 //string,num
 
+
 function square1(x) { 
     return x * x ; 
     } 
@@ -25,4 +26,23 @@ function add(num1,num2){
 }
 function subtract(num1,num2){
     return num2-num1
+}
+
+function calc(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else {
+        return subtract(num1, num2);
+    }
+}
+console.log(calc('+', 5, 3));
+console.log(calc('-', 5, 3));
+
+
+function divide(num1,num2){
+    return num2/num1
+}
+
+function multiply(num1,num2){
+    return num2*num1
 }
