bd1c98179ff81f394737b40aa071e3e1afc232b4
brushurteeth 8 <adem.hameed.123@gmail.com>
2024-02-11 17:32:58 +0100

    js

---
+++ main.js
@@ -0,0 +1,28 @@
+var carName = Volvo ;
+var length = 16; //number
+
+var lastName = "Johnson"; //string 
+
+var isGreaterThan10 = length > 10 //string,num
+
+function square1(x) { 
+    return x * x ; 
+    } 
+   
+    function square2 (x){ 
+    return x * x ; 
+    }
+
+    function square3(x)  { 
+        return x * x;
+        }
+        function largest(x,y) {
+            if(x>y){ return x} 
+            {return y}
+        }
+function add(num1,num2){
+    return num1+num2
+}
+function subtract(num1,num2){
+    return num2-num1
+}
